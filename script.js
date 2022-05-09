42dc4040e00b54dd1b0c940d759f9bb3746e8c09
albertofcyber98 <yohanesalbert2@gmail.com>
2022-05-09 20:56:56 +0800

    Soal Nomor 8

--- script.js
+++ script.js
@@ -45,15 +45,77 @@ const nomor4 = () => {
 }
 
 const nomor5 = () => {
-    let inputan = 104
+    let inputan = 23
+    let tampungString = ""
     if (inputan > 100 || inputan < 1) {
         console.log("Silahkan masukkan bilangan 1-100")
     }
+    let tampungData = Array.from(String(inputan), Number)
+    for (let angkaAwal = 0; angkaAwal < tampungData.length; angkaAwal++){
+        if (tampungData[angkaAwal] == 1 && tampungData.length == 2 ) {
+            tampungString += "Satu"
+        } else if (tampungData[angkaAwal] == 1 && tampungData.length == 1) {
+            tampungString += "Sepuluh "
+        } else if (tampungData[angkaAwal] == 2 && tampungData[angkaAwal] == 2) {
+            tampungString += "Dua"
+        } else if (tampungData[angkaAwal] == 2 && tampungData.length == 1) {
+            tampungString += "Dua Puluh "
+        }else if (tampungData[angkaAwal] == 3 && tampungData[angkaAwal] == 2) {
+            tampungString += "Tiga"
+        } else if (tampungData[angkaAwal] == 3 && tampungData.length == 2) {
+            tampungString += "Tiga Puluh "
+        }else if (tampungData[angkaAwal] == 4 && tampungData.length == 2) {
+            tampungString += "Empat Puluh "
+        } else if (tampungData[angkaAwal] == 4 && tampungData.length == 1) {
+            tampungString += "Empat"
+        }else if (tampungData[angkaAwal] == 5 && tampungData.length == 2) {
+            tampungString += "Lima Puluh "
+        } else if (tampungData[angkaAwal] == 5 && tampungData.length == 1) {
+            tampungString += "Lima"
+        }else if (tampungData[angkaAwal] == 6 && tampungData.length == 2) {
+            tampungString += "Enam Puluh "
+        } else if (tampungData[angkaAwal] == 6 && tampungData.length == 1) {
+            tampungString += "Enam"
+        }else if (tampungData[angkaAwal] == 7 && tampungData.length == 2) {
+            tampungString += "Tujuh Puluh "
+        } else if (tampungData[angkaAwal] == 7 && tampungData.length == 1) {
+            tampungString += "Tujuh"
+        }else if (tampungData[angkaAwal] == 8 && tampungData.length == 2) {
+            tampungString += "Tujuh Puluh "
+        } else if (tampungData[angkaAwal] == 8 && tampungData.length == 1) {
+            tampungString += "Tujuh"
+        }else if (tampungData[angkaAwal] == 9 && tampungData.length == 2) {
+            tampungString += "Tujuh Puluh "
+        } else if (tampungData[angkaAwal] == 9 && tampungData.length == 1) {
+            tampungString += "Tujuh"
+        }
+    }
+    console.log(tampungString)
 }
 
 const nomor6 = () => {
     let dataArray = [1, 4, 7, 9, 12];
 
+}
+
+const nomor7 = () => {
+    let dataArray = [1, 4, 7, 9, 12];
+    console.log(dataArray.length)
+}
+
+const nomor8 = () => {
+    let dataAngka = 15
+    for (let angkaAwal = 1; angkaAwal <= 15; angkaAwal++){
+        if (angkaAwal == 3) {
+            console.log('Edu')
+        } else if (angkaAwal == 9) {
+            console.log('Work')
+        } else if (angkaAwal == 15) {
+            console.log('EduWork')
+        } else {
+            console.log(angkaAwal)
+        }
+    }
 }
 
 const nomor9 = () => {
@@ -79,4 +141,4 @@ const nomor10 = () => {
         console.log(`${inputanTahun} bukan tahun kabisat`)
     }
 }
-nomor3()
+nomor8()
