34f88c20159ad79d404bd1d758b4a9f6b3de1ecf
albertofcyber98 <yohanesalbert2@gmail.com>
2022-05-09 19:22:13 +0800

    Soal Nomor 2

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let inputan = 8
-let tampungAngka = 1;
-for (let angkaAwal = 1; angkaAwal <= inputan; angkaAwal++){
-    tampungAngka *= angkaAwal
+const nomor1 = () => {
+    let inputan = 4
+    let tampungAngka = 1
+    for (let angkaAwal = 1; angkaAwal <= inputan; angkaAwal++){
+        tampungAngka *= angkaAwal
+    }
+    console.log(tampungAngka);
 }
-console.log(tampungAngka)
+
+const nomor2 = () => {
+    let input = "abcde"
+    let arrayTampung = input.split("")
+    let angkaAwal = 0;
+    let output = "";
+    let maxArray = arrayTampung.length-1
+    for (maxArray; maxArray >= angkaAwal; maxArray--){
+        output += arrayTampung[maxArray]
+    }
+    console.log(output)
+}
+nomor2()
